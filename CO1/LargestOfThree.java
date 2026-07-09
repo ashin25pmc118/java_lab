a219a31aaaeb6e249d6b396b58be826d082aa78e
ashinmathai33-hash <ashinmathai33@gmail.com>
2026-07-09 10:26:00 +0530

    CO1 - Task 13: Largest of Three Numbers

---
+++ CO1/LargestOfThree.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class LargestOfThree {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("A = ");
+        int a = scanner.nextInt();
+
+        System.out.print("B = ");
+        int b = scanner.nextInt();
+
+        System.out.print("C = ");
+        int c = scanner.nextInt();
+
+        int largest;
+        if (a >= b && a >= c) {
+            largest = a;
+        } else if (b >= a && b >= c) {
+            largest = b;
+        } else {
+            largest = c;
+        }
+
+        System.out.println("Largest = " + largest);
+
+        scanner.close();
+    }
+}
