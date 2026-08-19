221e64e7c7cea7dbc8db9921710dbc3523938230
ashinmathai33-hash <ashinmathai33@gmail.com>
2026-08-19 10:02:00 +0530

    Java_Lab_Exercises - Task 01: Default Constructor Demonstration

---
+++ Java_Lab_Exercises/StudentClass.java
@@ -0,0 +1,11 @@
+class Student {
+    public Student() {
+        System.out.println("Welcome to Student Class");
+    }
+}
+
+public class StudentClass {
+    public static void main(String[] args) {
+        Student s = new Student();
+    }
+}
